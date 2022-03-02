6573f5aa2767c0bd9b3243d4294c6b18e93a2ff6
Yonghyun Kim <lygon55555@gmail.com>
2022-03-02 10:39:10 +0900

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Yonghyun on 2022/03/02.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
